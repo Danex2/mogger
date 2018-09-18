90c0fe514acc0d1d7608c21984e7c75f495d0e25
Danex2 <khadane.miller@gmail.com>
2018-09-17 21:15:13 -0400

    finished form layout

--- mogger-front/src/components/Post.js
+++ mogger-front/src/components/Post.js
@@ -1,91 +1,199 @@
 import React, { Component } from 'react';
 
 class Post extends Component {
+  state = {
+    title: '',
+    image: '',
+    head: '',
+    shoulder: '',
+    chest: '',
+    back: '',
+    wrists: '',
+    hands: '',
+    waist: '',
+    legs: '',
+    feet: '',
+    weapon: '',
+    offhand: '',
+    notes: ''
+  };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state.shoulder);
+  };
   render() {
     return (
       <div className="container post-container d-flex align-items-center">
         <form>
           <h1 className="text-center">Post new transmog</h1>
-          <div class="row">
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Post title</label>
+          <div className="row">
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Post title</label>
               <input
+                onChange={this.handleChange}
+                name="title"
                 type="text"
-                class="form-control"
+                className="form-control"
                 placeholder="Name your transmog post"
               />
             </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Image Link</label>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Image Link</label>
               <input
+                onChange={this.handleChange}
+                name="image"
                 type="text"
-                class="form-control"
+                className="form-control"
                 placeholder="Image link"
               />
             </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Head</label>
-              <input type="text" class="form-control" placeholder="Head" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Shoulder</label>
-              <input type="text" class="form-control" placeholder="Shoulder" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Chest</label>
-              <input type="text" class="form-control" placeholder="Chest" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Back</label>
-              <input type="text" class="form-control" placeholder="Back" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Wrists</label>
-              <input type="text" class="form-control" placeholder="Wrists" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Hands</label>
-              <input type="text" class="form-control" placeholder="Hands" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Waist</label>
-              <input type="text" class="form-control" placeholder="Waist" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Legs</label>
-              <input type="text" class="form-control" placeholder="Legs" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Feet</label>
-              <input type="text" class="form-control" placeholder="Feet" />
-            </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Weapon</label>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Head</label>
               <input
+                onChange={this.handleChange}
                 type="text"
-                class="form-control"
+                className="form-control"
+                placeholder="Head"
+                name="head"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Shoulder</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Shoulder"
+                name="shoulder"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Chest</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Chest"
+                name="chest"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Back</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Back"
+                name="back"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Wrists</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Wrists"
+                name="wrists"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Hands</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Hands"
+                name="hands"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Waist</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Waist"
+                name="waist"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Legs</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Legs"
+                name="legs"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Feet</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Feet"
+                name="feet"
+              />
+            </div>
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Weapon</label>
+              <input
+                onChange={this.handleChange}
+                name="weapon"
+                type="text"
+                className="form-control"
                 placeholder="Mainhand/2Hand"
               />
             </div>
-            <div class="col-lg-12 mb-3">
-              <label for="formGroupExampleInput2">Off-hand</label>
-              <input type="text" class="form-control" placeholder="Off-hand" />
+            <div className="col-lg-12 mb-3">
+              <label htmlFor="formGroupExampleInput2">Off-hand</label>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                placeholder="Off-hand"
+                name="offhand"
+              />
             </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Additional Notes</label>
+            <div className="col-lg-12 mb-3">
+              <label htmlFor="formGroupExampleInput2">Additional Notes</label>
               <textarea
-                class="form-control"
+                onChange={this.handleChange}
+                name="notes"
+                className="form-control"
                 placeholder="Any other important information"
                 rows="4"
               />
             </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Class</label>
-              <input type="text" class="form-control" placeholder="Last name" />
+            <div className="col-lg-6 mb-5">
+              <label htmlFor="formGroupExampleInput2">Class</label>
+              <select class="form-control form-control-sm">
+                <option>Class</option>
+                <option>Mage</option>
+                <option>Hunter</option>
+                <option>Rogue</option>
+                <option>Paladin</option>
+                <option>Shaman</option>
+                <option>Druid</option>
+                <option>Warlock</option>
+                <option>Priest</option>
+                <option>Warrior</option>
+                <option>Death Knight</option>
+                <option>Monk</option>
+                <option>Demon Hunter</option>
+              </select>
             </div>
-            <div class="col-lg-6 mb-3">
-              <label for="formGroupExampleInput2">Armor Type</label>
-              <input type="text" class="form-control" placeholder="Last name" />
+            <div className="col-lg-6 mb-3">
+              <label htmlFor="formGroupExampleInput2">Armor Type</label>
+              <select class="form-control form-control-sm">
+                <option>Armor Type</option>
+                <option>Cloth</option>
+                <option>Leather</option>
+                <option>Plate</option>
+                <option>Mail</option>
+              </select>
             </div>
           </div>
         </form>
