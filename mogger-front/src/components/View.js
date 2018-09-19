80f1aa596ef014980ac724ccc14dd07d4ff9601e
Danex2 <khadane.miller@gmail.com>
2018-09-19 19:32:25 -0400

    removed layout from view and viewitem/redoing

--- mogger-front/src/components/View.js
+++ mogger-front/src/components/View.js
@@ -7,31 +7,8 @@ class View extends React.Component {
   }
   render() {
     return (
-      <div className="card-container d-flex justify-content-center">
-        {this.props.data.map(item => {
-          return (
-            <div
-              key={item.id}
-              className="card col-lg-3 col-sm-10 col-md-9 mb-3"
-            >
-              <h5 className="card-title d-flex justify-content-center mt-2">
-                {item.title}
-              </h5>
-              <img
-                className="card-img-top mt-1 container rounded h-75"
-                src={item.imgLink}
-              />
-              <div className="card-body d-flex justify-content-center">
-                <Link
-                  to={`/view/${item.id}`}
-                  className="btn btn-primary align-self-end"
-                >
-                  View full transmog
-                </Link>
-              </div>
-            </div>
-          );
-        })}
+      <div>
+        <h1>placeholder</h1>
       </div>
     );
   }
