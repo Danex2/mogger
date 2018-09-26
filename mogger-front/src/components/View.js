8f624293e9bf488da5995e42e8ddb76e9d9948f1
Danex2 <khadane.miller@gmail.com>
2018-09-26 19:17:32 -0400

    i hate css

--- mogger-front/src/components/View.js
+++ mogger-front/src/components/View.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 class View extends React.Component {
   constructor(props) {
@@ -7,25 +8,31 @@ class View extends React.Component {
   }
   render() {
     const { data } = this.props;
-    return data.map(item => {
-      return (
-        <div key={item.id} className="row container">
-          <div className="col s12 m7 l4">
-            <div className="card">
-              <div className="card-image">
-                <img src={item.imgLink} />
-              </div>
-              <div className="card-content center-align">
-                <p>{item.title}</p>
-              </div>
-              <div className="card-action center-align">
-                <Link to={`/view/${item.id}`}>View this transmog</Link>
+    console.log(data);
+    return (
+      <div className="row container">
+        {data.map(item => {
+          return (
+            <div key={item.id} className="col s12 m6 l4">
+              <div className="card">
+                <div className="card-image">
+                  <img src={item.imgLink} />
+                </div>
+                <div className="card-content">
+                  <p className="center-align">{item.title}</p>
+                </div>
+                <div className="card-action left-align">
+                  <Link to={`/view/${item.id}`}>View full transmog</Link>
+                  <span className="right-align">
+                    Class: {item.class} Type: {item.armorType}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      );
-    });
+          );
+        })}
+      </div>
+    );
   }
 }
 
