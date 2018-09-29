1cfbb09319ecd1dbd6cecc7226275c43697b50df
Danex2 <khadane.miller@gmail.com>
2018-09-28 23:41:04 -0400

    :bug: Fixed single item view
    css changes

--- mogger-front/src/components/ViewItem.js
+++ mogger-front/src/components/ViewItem.js
@@ -15,29 +15,30 @@ class ViewItem extends Component {
   render() {
     const { data } = this.state;
     return (
-      <div className="container form-container">
-        <div className="col s12 m7">
-          <div className="card">
-            <div className="card-image">
-              <img src={data.imgLink} />
-              <span className="card-title">{data.title}</span>
-            </div>
-            <div className="card-content">
-              <p>{data.notes}</p>
-            </div>
-            <div className="card-action center-align">
-              <div>Head: {data.head}</div>
-              <div>Shoulder: {data.shoulder}</div>
-              <div>Chest: {data.chest}</div>
-              <div>Back: {data.back}</div>
-              <div>Wrists: {data.wrists}</div>
-              <div>Hands: {data.hands}</div>
-              <div>Waist: {data.waist}</div>
-              <div>Legs: {data.legs}</div>
-              <div>Feet: {data.feet}</div>
-              <div>Weapon: {data.weapon}</div>
-              <div>
-                Off-hand: {!data.weapon2 && <span>No item specified</span>}
+      <div className="center-align row">
+        <div className="container form-container">
+          <div className="col s12 m2 l6 offset-l3">
+            <div className="card">
+              <div className="card-image">
+                <img src={data.imgLink} />
+                <span className="card-title">{data.title}</span>
+              </div>
+              <div className="card-content center-align">
+                <div>Head: {data.head}</div>
+                <div>Shoulder: {data.shoulder}</div>
+                <div>Chest: {data.chest}</div>
+                <div>Back: {data.back}</div>
+                <div>Wrists: {data.wrists}</div>
+                <div>Hands: {data.hands}</div>
+                <div>Waist: {data.waist}</div>
+                <div>Legs: {data.legs}</div>
+                <div>Feet: {data.feet}</div>
+                <div>Weapon: {data.weapon}</div>
+                <div>Off-hand: {data.weapon2}</div>
+              </div>
+              <div className="card-action center-align">
+                <h5>Extra notes</h5>
+                <p>{data.notes}</p>
               </div>
             </div>
           </div>
