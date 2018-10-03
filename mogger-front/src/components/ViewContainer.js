811eb7b72c00203bed3a4ce2d971e51aad95858e
Danex2 <khadane.miller@gmail.com>
2018-10-03 11:09:57 -0400

    added spinner loading icon

--- mogger-front/src/components/ViewContainer.js
+++ mogger-front/src/components/ViewContainer.js
@@ -2,19 +2,29 @@ import React, { Component } from "react";
 import axios from "axios";
 import "../App.css";
 import View from "../components/View";
+import Spinner from "./Spinner";
 
 class ViewContainer extends Component {
   state = {
-    data: []
+    data: [],
+    loading: true
   };
 
   componentDidMount() {
     axios.get("/api/view/all").then(res => {
-      this.setState({ data: res.data });
+      this.setState({ data: res.data, loading: false });
     });
   }
   render() {
-    const { data } = this.state;
+    const { data, loading } = this.state;
+    if (loading) {
+      return (
+        <div className="spinner-container">
+          <Spinner />
+        </div>
+      );
+    }
+
     return <View data={data} />;
   }
 }
