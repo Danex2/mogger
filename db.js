d8778210c2bc6c7cade8790b6e604856b744bc92
Danex2 <khadane.miller@gmail.com>
2018-11-29 14:56:28 -0500

    removed some bad dependencies

--- db.js
+++ db.js
@@ -3,22 +3,7 @@ const PostModel = require("./models/Post");
 require("dotenv").config();
 
 // Creating the connection to the database [dbname, user, password]
-const sequelize = new Sequelize(
-  process.env.DB_TABLE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_IP,
-    dialect: "postgres",
-    port: 5432,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const Post = PostModel(sequelize, Sequelize);
 
